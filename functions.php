2e6da98b2b7ee4d14cb8ea7fbe2fcefba3f1f951
caitlyn4444 <caitlynmiller220@gmail.com>
2023-03-25 20:31:18 +0800

    Nav changes

--- functions.php
+++ functions.php
@@ -10,7 +10,22 @@ function load_my_scripts()
     wp_enqueue_style('style-css', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('test', get_stylesheet_directory_uri() . '/responsive.css');
 
-
 }
 add_action('wp_enqueue_scripts', 'load_my_scripts');
+?>
+
+<?php
+function register_navwalker()
+{
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action('after_setup_theme', 'register_navwalker');
+?>
+
+<?php
+register_nav_menus(
+    array(
+        'primary' => __('Primary Menu', 'top-menu'),
+    )
+);
 ?>
--- page_header.php
+++ page_header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -45,38 +44,40 @@
 </head>
 
 <body>
-    <header class="container-fluid bg1">
+    <header class="container-fluid smallpage-bg">
         <div class="container">
             <nav class="navbar navbar-expand-md navbar-dark">
                 <!-- Brand -->
                 <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
 
                 <!-- Toggler/collapsibe Button -->
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1"
-                    aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <?php
-
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'primary',
-                        'depth' => 2,
-                        // 1 = no dropdowns, 2 = with dropdowns.
-                        'container' => 'div',
-                        'container_class' => 'collapse navbar-collapse',
-                        'container_id' => 'bs-example-navbar-collapse-1',
-                        'menu_class' => 'navbar-nav ml-auto',
-                        'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-                        'walker' => new WP_Bootstrap_Navwalker(),
-                    ));
-
-                ?>
+                <!-- Navbar links -->
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item text-right">
+                            <a class="nav-link" href="#">HOME</a>
+                        </li>
+                        <li class="nav-item text-right">
+                            <a class="nav-link" href="#">PROJECTS</a>
+                        </li>
+                        <li class="nav-item text-right">
+                            <a class="nav-link" href="#">PUBLICATIONS</a>
+                        </li>
+                        <li class="nav-item text-right">
+                            <a class="nav-link" href="#">CV</a>
+                        </li>
+                        <li class="nav-item text-right">
+                            <a class="nav-link" href="#">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
-            <div class="tag">
-                <h1>Games Design For the Future</h1>
+            <div class="tag-smallpage-header">
+                <h1 class="smallpage-title">Games Design For the Future</h1>
             </div>
 
 
